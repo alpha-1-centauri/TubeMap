511375cd6412b2b4f2a0a837893569d78639483b
Elliot Lewis <elliotleelewis@gmail.com>
2017-02-06 17:55:56 +0000

    Major Changes
    * Switched from ‘create-react-scripts’ to Gulp.
    * Migrated stylesheets to SCSS.
    * Added search functionality.
    * Added markers and tooltips for each of the stations.

--- README.md
+++ README.md
@@ -11,5 +11,5 @@ TubeMap is an interactive web-based map, of the London Underground.
 * Copy `env-example.json` to `env.json`
 * Sign up for a [Mapbox](https://www.mapbox.com/) key,  and add it to `env.json`
 * Run `npm install`
-* Run `npm start`
-* Go to `http://localhost:3000` in a browser
+* Run `npm start` or `gulp`
+* Open `index.htm` in a browser
---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+const gulp = require('gulp');
+const fs = require('fs');
+const browserify = require('browserify');
+const sass = require('gulp-sass');
+gulp.task('js', function() {
+	return browserify('src/index.js')
+		.transform("babelify", {presets: ['es2015']})
+		.bundle()
+		.pipe(fs.createWriteStream('build/index.js'));
+});
+gulp.task('sass', function() {
+	return gulp.src('src/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('build'));
+});
+gulp.task('watch', ['default'], function() {
+	gulp.watch('src/*.js', ['js']);
+	gulp.watch('src/*.scss', ['sass']);
+});
+gulp.task('default', ['js', 'sass']);
